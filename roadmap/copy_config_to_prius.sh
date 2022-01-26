caa07b9ab3e3bc81b7c2591437b0398405d017ef
Oscar de Groot <o.m.degroot@tudelft.nl>
2022-01-26 11:53:39 +0100

    added file for copying config files to prius_launch

---
+++ roadmap/copy_config_to_prius.sh
@@ -0,0 +1,3 @@
+echo "Copying files into prius_launch config"
+cp config/settings.yaml ../../prius_launch/launch/control/roadmap/settings.yaml
+cp launch/roadmap.launch ../../prius_launch/launch/control/roadmap/roadmap.launch
