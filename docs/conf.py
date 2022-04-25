cfce75b324bd0d9253fe2fd6c33984639fd6fd4c
Oscar de Groot <o.m.degroot@tudelft.nl>
2022-04-25 16:30:48 +0200

    updated documentation files.

--- docs/conf.py
+++ docs/conf.py
@@ -213,4 +213,4 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('cd ../doxygen; doxygen Doxyfile', shell=True)
